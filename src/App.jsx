505bca7d62091c202af13da2c95c11a2d6efe484
Dácio Lima <daciolima@MacBook-Pro.local>
2020-12-12 00:46:03 -0300

    Projeto configurado

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css";
+import Menu from "./components/layout/Menu";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Menu />
+    </div>
+  );
+}
---
+++ src/components/layout/Menu.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Menu.css";
+
+export default function Menu() {
+  return (
+    <div className="Menu">
+      <h1>Componente Menu</h1>
+    </div>
+  );
+}
